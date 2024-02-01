fe7517d4950c1addb04e877b889b35a71778ad09
fernandokkang <110192117+fernandokkang@users.noreply.github.com>
2024-02-02 01:17:57 +0900

    [level 2] Title: 예상 대진표, Time: 0.22 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        
+        while(a != b) {
+            
+            a = (int) Math.ceil((double) a/2);
+            b = (int) Math.ceil((double) b/2);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
