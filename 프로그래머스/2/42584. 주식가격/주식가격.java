7482ba955154086aef780ff5175db2c413a6766b
Sunghoon Kang <110192117+fernandokkang@users.noreply.github.com>
2024-06-22 19:52:23 +0900

    [level 2] Title: 주식가격, Time: 23.12 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,17 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
         
-        for(int i = 0; i < prices.length; i++) {
+        int n = prices.length;
+        
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[n];
+        
+        for(int i = 0; i < n; i++){
             
-            for(int j = i + 1; j < prices.length; j++) {
-        
-                answer[i]++;
-                
-                if(prices[j] < prices[i]) {
-                    break;
-                }
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
+            stack.push(i);
+        }
+        
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - idx - 1;
         }
         
         return answer;
