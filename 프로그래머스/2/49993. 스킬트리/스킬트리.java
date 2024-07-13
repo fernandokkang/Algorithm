ae3fde0fea7fae76b63cd545ef7a425c0325860a
Sunghoon Kang <110192117+fernandokkang@users.noreply.github.com>
2024-07-14 00:48:07 +0900

    [level 2] Title: 스킬트리, Time: 9.45 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,30 +2,16 @@ class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
-        for(String skill_tree : skill_trees) {
-            if (isValidSkillTree(skill, skill_tree)) {
+        String regex = "[^" + skill + "]";
+
+        for (String skill_tree : skill_trees) {
+            String filteredSkillTree = skill_tree.replaceAll(regex, "");
+
+            if (skill.startsWith(filteredSkillTree)) {
                 answer++;
             }
         }
         
         return answer;
-    }
-    
-    private boolean isValidSkillTree(String skill, String skill_tree) {
-        int skillIndex = 0;
-
-        for (char c : skill_tree.toCharArray()) {
-            int index = skill.indexOf(c);
-            if (index == -1) {
-                continue;
-            }
-            if (index == skillIndex) {
-                skillIndex++;
-            } else {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
