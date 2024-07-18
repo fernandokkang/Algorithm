9f5dd54dd93645e8a791a46aa473c022a53c5f4e
Sunghoon Kang <110192117+fernandokkang@users.noreply.github.com>
2024-07-18 23:57:12 +0900

    [level 3] Title: 숫자 게임, Time: 66.12 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int aIdx = 0;
+        int bIdx = 0;
+        
+        for(int i=0; i<A.length; i++) {
+            if(A[aIdx] < B[bIdx]) {
+                aIdx++;
+                answer++;
+            }
+            
+            bIdx++;
+        }
+        
+        return answer;
+    }
+}
