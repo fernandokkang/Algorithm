a3231d40eb8395ac2ca8435084cbae1329f3cd47
Sunghoon Kang <110192117+fernandokkang@users.noreply.github.com>
2024-07-22 22:48:40 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> numbers = new ArrayList<>();
+        long[] factorial = new long[n + 1];
+        
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+            numbers.add(i);
+        }
+        
+        k--;
+
+        for (int i = 0; i < n; i++) {
+            int index = (int)(k / factorial[n - 1 - i]);
+            answer[i] = numbers.get(index);
+            numbers.remove(index);
+            k %= factorial[n - 1 - i];
+        }
+        
+        return answer;
+    }
+}
