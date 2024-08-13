15cba212f06a738e4ae8a0b4aa436886641cbd26
Sunghoon Kang <110192117+fernandokkang@users.noreply.github.com>
2024-08-13 23:38:39 +0900

    [Gold V] Title: LCS, Time: 236 ms, Memory: 21684 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+			
+			String str1 = sc.next();
+			String str2 = sc.next();
+			
+			System.out.println(LCS(str1, str2));
+		
+	}
+
+	public static int LCS(String str1, String str2) {
+		
+		int n = str1.length();
+		int m = str2.length();
+		
+		int [][] dp = new int[n+1][m+1];
+		
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=m; j++) {
+				
+				if(str1.charAt(i-1) == str2.charAt(j-1)) dp[i][j] = dp[i-1][j-1] + 1;
+				else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+			}
+		}
+		
+		return dp[n][m];
+	}
+}
