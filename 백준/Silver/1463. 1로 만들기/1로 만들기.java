7a77d009a234940bcd42d67d36cbdb3cab29cbc8
Sunghoon Kang <110192117+fernandokkang@users.noreply.github.com>
2024-08-14 23:04:45 +0900

    [Silver III] Title: 1로 만들기, Time: 180 ms, Memory: 21548 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+        
+		System.out.println(findMinCal(N));
+	}
+	
+	public static int findMinCal(int n) {
+		
+		int [] dp = new int[n+1];
+		
+		for(int i=2; i<=n; i++) {
+			
+			dp[i] = dp[i-1] + 1;
+			
+			if(i%3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3] + 1);
+			}
+			
+			if(i%2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2] + 1);
+			}
+		}
+		
+		return dp[n];
+	}
+}
